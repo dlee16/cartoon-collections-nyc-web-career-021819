712fe5e2b265e55f0cf2687befe89cfd1bf44cc3
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 22:49:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
   types = cheese_types.join("")
-  cheese.include?(cheese_types.join(""))
+  cheese.include?(types)
 end
 end
