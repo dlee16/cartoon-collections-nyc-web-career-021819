f7b53ae7c8d1307d600c4a734725551fe1cbf1ae
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-04 22:20:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name,index|
+    puts "#{index +1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(element)
   # Your code here
 end
 
